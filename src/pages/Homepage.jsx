3285b92981a1a8729cffe0624cc75b98f52f4695
adityavarmachandika <adithyavarmachandika@gmail.com>
2024-12-12 23:27:46 +0530

    front end completion

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
+import Todotile from '../components/Todotile'
 import tailwindConfig from '../../tailwind.config'
 const Homepage = () => {
   return (
-
-    <div className='font-bold mx-6'>
+    <div className='font-bold'>
 
       <div className='m-9 text-center text-5xl font-bold'>TODO LIST</div>
+      <div className='flex justify-around'>
+        <div className='flex flex-col w-1/6'>
+        <h1 className='self-center text-2xl'>ADD TASK</h1>
+          <label htmlFor="taskInput" className='text-lg mb-3'>NAME</label>
+          <input type="text" className='h-10 shadow-lg text-ellipsis rounded-lg bg-slate-100 focus:outline-none focus:ring-2 focus:ring-red-500' placeholder=''/>
+          <label htmlFor="taskInput" className='text-lg my-3'>DESCRITION</label>
+          <input type="text" className='shadow-lg h-24 rounded-lg bg-slate-100 focus:outline-none focus:ring-2 focus:ring-red-500' placeholder=' '/>
+          <button  className='w-20 bg-red-400 self-end m-5 rounded-xl shadow-sm text-xl' >ADD</button>
+        </div>
       <div>
-        <p >ADD NEW TASK</p>
-        
+        <Todotile />
       </div>
+      </div>
+
     </div>
   )
 }
